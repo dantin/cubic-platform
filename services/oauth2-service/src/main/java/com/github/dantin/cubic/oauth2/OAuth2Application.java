aa86275438dff5f8f71ff8639faf43baa0e28b8b
dantin <chengjie.ding@gmail.com>
2020-04-20 13:03:53 +0800

    add discovery serivce.

--- services/oauth2-service/src/main/java/com/github/dantin/cubic/oauth2/OAuth2Application.java
+++ services/oauth2-service/src/main/java/com/github/dantin/cubic/oauth2/OAuth2Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServerApplication {
+public class OAuth2Application {
   public static void main(String[] args) {
-    SpringApplication.run(AuthServerApplication.class, args);
+    SpringApplication.run(OAuth2Application.class, args);
   }
 }
