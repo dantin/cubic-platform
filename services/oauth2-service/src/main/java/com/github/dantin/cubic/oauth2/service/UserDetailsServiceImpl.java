eed9feb018fa7480a076da34807db9cc5156f450
dantin <chengjie.ding@gmail.com>
2020-04-22 09:51:16 +0800

    adjust user path.

--- services/oauth2-service/src/main/java/com/github/dantin/cubic/oauth2/service/UserDetailsServiceImpl.java
+++ services/oauth2-service/src/main/java/com/github/dantin/cubic/oauth2/service/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package com.github.dantin.cubic.oauth2.service;
 
 import com.github.dantin.cubic.protocol.user.UserDto;
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -15,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
   private final RestTemplate restTemplate;
 
   public UserDetailsServiceImpl(RestTemplate restTemplate) {
@@ -23,20 +27,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    ResponseEntity<UserDto> userResponse =
+    ResponseEntity<UserDto> response =
         restTemplate.exchange(
             "http://user-service/users/" + username, HttpMethod.GET, null, UserDto.class);
-    if (!userResponse.getStatusCode().is2xxSuccessful() || Objects.isNull(userResponse.getBody())) {
+    if (!response.getStatusCode().is2xxSuccessful() || Objects.isNull(response.getBody())) {
+      LOGGER.warn("user-service went wrong with http status code {}", response.getStatusCode());
       throw new UsernameNotFoundException("user " + username + " not found");
     }
-    UserDto user = userResponse.getBody();
+    UserDto entity = response.getBody();
     return new User(
-        user.getUsername(),
-        user.getPassword(),
-        user.getEnabled(),
+        entity.getUsername(),
+        entity.getPassword(),
+        entity.getEnabled(),
         true,
         true,
         true,
-        AuthorityUtils.createAuthorityList(user.getRole()));
+        AuthorityUtils.createAuthorityList(entity.getRole()));
   }
 }
