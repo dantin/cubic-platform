a82ff364364efabd4053aef4ca477b492928dc89
dantin <chengjie.ding@gmail.com>
2020-04-24 13:37:22 +0800

    add schema and data script.

---
+++ data/postgres-entrypoint/room-1-schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE room_db WITH ENCODING='utf-8';
+GRANT ALL PRIVILEGES ON DATABASE room_db TO postgres;
+
+\connect room_db;
+
+-- t_room
+CREATE TABLE IF NOT EXISTS t_room (
+    id VARCHAR(32) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- t_room_allocation
+CREATE TABLE IF NOT EXISTS t_room_allocation (
+    user_id VARCHAR(32) NOT NULL,
+    room_id VARCHAR(32) NOT NULL,
+    PRIMARY KEY(user_id, room_id)
+);
+
+-- t_stream
+CREATE TABLE IF NOT EXISTS t_stream (
+    id VARCHAR(32) NOT NULL,
+    type VARCHAR(32) NOT NULL,
+    role VARCHAR(32) NOT NULL,
+    protocol VARCHAR(16) NOT NULL,
+    host VARCHAR(255) NOT NULL,
+    port INTEGER NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    room_id VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
