a82bc4f2dd02c6d7084c21713cbae319204971ec
dantin <chengjie.ding@gmail.com>
2020-04-24 11:52:20 +0800

    add test case.

--- services/room-service/src/test/java/com/github/dantin/cubic/room/RoomApplicationMvcTest.java
+++ services/room-service/src/test/java/com/github/dantin/cubic/room/RoomApplicationMvcTest.java
@@ -1,5 +1,6 @@
 package com.github.dantin.cubic.room;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -20,6 +22,8 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import redis.embedded.RedisServer;
 
 @RunWith(SpringRunner.class)
@@ -52,6 +56,8 @@ public class RoomApplicationMvcTest {
   @Test
   public void basicCrud() throws Exception {
     getRoom("11");
+    int size = listRoom(4);
+    assertEquals(size, 4);
   }
 
   private void getRoom(String roomId) throws Exception {
@@ -62,5 +68,33 @@ public class RoomApplicationMvcTest {
                     .contentType(MediaType.APPLICATION_JSON)
                     .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
+
+    String resultString = result.andReturn().getResponse().getContentAsString();
+
+    JacksonJsonParser jsonParser = new JacksonJsonParser();
+    String name = jsonParser.parseMap(resultString).get("name").toString();
+    assertEquals("Room-01", name);
+  }
+
+  private int listRoom(int pageSize) throws Exception {
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("n", "1");
+    params.add("s", String.valueOf(pageSize));
+
+    ResultActions result =
+        mockMvc
+            .perform(
+                get("/rooms")
+                    .params(params)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+    String resultString = result.andReturn().getResponse().getContentAsString();
+
+    JacksonJsonParser jsonParser = new JacksonJsonParser();
+    String size = jsonParser.parseMap(resultString).get("size").toString();
+
+    return Integer.parseInt(size);
   }
 }
