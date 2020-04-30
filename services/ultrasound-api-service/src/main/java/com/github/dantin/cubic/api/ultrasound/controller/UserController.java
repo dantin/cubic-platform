06b40247f5f258484e51da0032d14c756ec22417
dantin <chengjie.ding@gmail.com>
2020-04-30 17:38:22 +0800

    integrate sso.

--- services/ultrasound-api-service/src/main/java/com/github/dantin/cubic/api/ultrasound/controller/UserController.java
+++ services/ultrasound-api-service/src/main/java/com/github/dantin/cubic/api/ultrasound/controller/UserController.java
@@ -2,12 +2,13 @@ package com.github.dantin.cubic.api.ultrasound.controller;
 
 import com.github.dantin.cubic.base.CollectionsHelper;
 import java.util.Map;
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import org.keycloak.KeycloakSecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +19,25 @@ public class UserController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
+  private final HttpServletRequest request;
+
+  @Autowired
+  public UserController(HttpServletRequest request) {
+    this.request = request;
+  }
+
   @GetMapping("/profile")
-  @PreAuthorize("hasAuthority('SCOPE_read')")
-  public ResponseEntity<Map<String, Object>> profile(@AuthenticationPrincipal Jwt principal) {
+  @RolesAllowed({"ultrasound-user", "ultrasound-admin", "ultrasound-root"})
+  public ResponseEntity<Map<String, Object>> profile() {
+    KeycloakSecurityContext context = getKeycloakSecurityContext();
+    String username = context.getToken().getPreferredUsername();
     if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug("get user profile of '{}'", principal.getClaimAsString("preferred_username"));
+      LOGGER.debug("get user profile of '{}'", username);
     }
-    return ResponseEntity.ok(
-        CollectionsHelper.mapOf(
-            "username",
-            principal.getClaimAsString("preferred_username"),
-            "organization",
-            principal.getClaimAsString("organization")));
+    return ResponseEntity.ok(CollectionsHelper.mapOf("username", username));
+  }
+
+  private KeycloakSecurityContext getKeycloakSecurityContext() {
+    return (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
   }
 }
