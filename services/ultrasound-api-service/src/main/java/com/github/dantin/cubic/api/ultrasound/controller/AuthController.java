a01c853da5e5d495715faea70df952453428c8ee
David Ding <chengjie.ding@gmail.com>
2020-05-11 13:48:55 +0800

    add login api. (#20)

---
+++ services/ultrasound-api-service/src/main/java/com/github/dantin/cubic/api/ultrasound/controller/AuthController.java
@@ -0,0 +1,68 @@
+package com.github.dantin.cubic.api.ultrasound.controller;
+
+import com.github.dantin.cubic.protocol.ultrasound.LoginRequest;
+import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
+  @Value("${keycloak.resource}")
+  private String clientId;
+
+  @Value("${keycloak.credentials.secret}")
+  private String clientSecret;
+
+  @Value("${keycloak.auth-server-url}")
+  private String authServerUrl;
+
+  @Value("${keycloak.realm}")
+  private String realm;
+
+  private final RestTemplate restTemplate;
+
+  public AuthController(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+    LOGGER.info("user '{}' login", request.getUsername());
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("client_id", clientId);
+    params.add("grant_type", "password");
+    params.add("client_secret", clientSecret);
+    params.add("scope", "openid");
+    params.add("username", request.getUsername());
+    params.add("password", request.getPassword());
+
+    HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<>(params, headers);
+    String tokenUrl =
+        String.format("%s/realms/%s/protocol/openid-connect/token", authServerUrl, realm);
+    ResponseEntity<String> response =
+        restTemplate.exchange(tokenUrl, HttpMethod.POST, body, String.class);
+    if (!response.getStatusCode().is2xxSuccessful() || Objects.isNull(response.getBody())) {
+      LOGGER.warn("oauth failed with status code {}", response.getStatusCode());
+    }
+    return response;
+  }
+}
--- services/ultrasound-api-service/src/test/java/com/github/dantin/cubic/api/ultrasound/UltrasoundApiMvcTest.java
+++ services/ultrasound-api-service/src/test/java/com/github/dantin/cubic/api/ultrasound/UltrasoundApiMvcTest.java
@@ -1,11 +1,18 @@
 package com.github.dantin.cubic.api.ultrasound;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dantin.cubic.protocol.ultrasound.LoginRequest;
 import com.tngtech.keycloakmock.api.KeycloakVerificationMock;
 import com.tngtech.keycloakmock.api.TokenConfig;
+import java.net.URI;
 import java.time.Instant;
 import java.util.Objects;
 import org.junit.AfterClass;
@@ -20,30 +27,39 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class UltrasoundApiApplicationIntegrationTest {
+public class UltrasoundApiMvcTest {
+
+  private static KeycloakVerificationMock keycloakMock;
+
+  private ObjectMapper objectMapper = new ObjectMapper();
+  private MockRestServiceServer mockServer;
 
   @Autowired private WebApplicationContext context;
 
   @Autowired private MockMvc mockMvc;
+
+  @Autowired private RestTemplate restTemplate;
 
   @Value("${keycloak.resource}")
   private String clientId;
 
   @Value("${keycloak.credentials.secret}")
   private String clientSecret;
-
-  private static KeycloakVerificationMock keycloakMock;
 
   @BeforeClass
   public static void prepare() {
@@ -59,9 +75,41 @@ public class UltrasoundApiApplicationIntegrationTest {
   }
 
   @Before
+  public void init() {
+    mockServer = MockRestServiceServer.createServer(restTemplate);
+  }
+
+  @Before
   public void setUp() {
     // .alwaysDo(print())
     mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+  }
+
+  @Test
+  public void doLogin_thenSuccess() throws Exception {
+    mockServer
+        .expect(
+            ExpectedCount.once(),
+            requestTo(
+                new URI(
+                    "http://localhost:8083/auth/realms/ultrasound/protocol/openid-connect/token")))
+        .andExpect(method(HttpMethod.POST))
+        .andRespond(
+            withStatus(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body("success"));
+    LoginRequest loginRequest = new LoginRequest();
+    loginRequest.setUsername("room01");
+    loginRequest.setPassword("password");
+    MvcResult result =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders.post("/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(asJsonString(loginRequest)))
+            .andExpect(status().isOk())
+            .andReturn();
+    String resultString = result.getResponse().getContentAsString();
+    assertEquals("success", resultString);
   }
 
   @Test
@@ -77,6 +125,14 @@ public class UltrasoundApiApplicationIntegrationTest {
             .andReturn();
     String resultString = result.getResponse().getContentAsString();
     assertNotNull(resultString);
+  }
+
+  private String asJsonString(final Object obj) {
+    try {
+      return objectMapper.writeValueAsString(obj);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   private String obtainMockAccessToken(String username) {
