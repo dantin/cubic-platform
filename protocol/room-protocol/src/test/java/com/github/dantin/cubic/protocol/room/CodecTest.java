a786935a7138026a295f8eb4fd39b875db586fa7
dantin <chengjie.ding@gmail.com>
2020-05-13 14:34:44 +0800

    move test case to room-protocol.

--- protocol/room-protocol/src/test/java/com/github/dantin/cubic/protocol/room/CodecTest.java
+++ protocol/room-protocol/src/test/java/com/github/dantin/cubic/protocol/room/CodecTest.java
@@ -1,5 +1,6 @@
 package com.github.dantin.cubic.protocol.room;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -8,6 +9,7 @@ import static org.junit.Assert.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.UUID;
 import org.junit.Test;
 
 public class CodecTest {
@@ -46,6 +48,56 @@ public class CodecTest {
     assertThat(actual.getName(), is(name));
     assertEquals(1, actual.getStreams().size());
     assertThat(stream, is(actual.getStreams().get(0)));
+  }
+
+  @Test
+  public void marshalPagination_thenSuccess() {
+    final int pages = 10;
+    final int page = 1;
+    final int size = 1;
+
+    RoutePage.Builder builder = RoutePage.builder();
+    builder.pages(pages).page(page).size(size);
+    for (int i = 0; i < size; i++) {
+      int id = page * size + i;
+      Route.Builder route = Route.builder().id(String.valueOf(id)).name("route " + id);
+      route.addStream(
+          Stream.builder()
+              .role(Role.ADMIN.getAlias())
+              .type(Device.CAMERA.getAlias())
+              .uri("srt::" + UUID.randomUUID().toString())
+              .build());
+      route.addStream(
+          Stream.builder()
+              .role(Role.ADMIN.getAlias())
+              .type(Device.DEVICE.getAlias())
+              .uri("srt::" + UUID.randomUUID().toString())
+              .build());
+      route.addStream(
+          Stream.builder()
+              .role(Role.USER.getAlias())
+              .type(Device.CAMERA.getAlias())
+              .uri("srt::" + UUID.randomUUID().toString())
+              .build());
+      route.addStream(
+          Stream.builder()
+              .role(Role.USER.getAlias())
+              .type(Device.DEVICE.getAlias())
+              .uri("srt::" + UUID.randomUUID().toString())
+              .build());
+      builder.addRoute(route.build());
+    }
+
+    RoutePage orig = builder.build();
+    RoutePage actual = Utility.marshal(orig, RoutePage.class);
+
+    assertNotNull(actual);
+    assertEquals(page, actual.getPage());
+    assertEquals(pages, actual.getPages());
+    assertEquals(size, actual.getRoutes().size());
+    assertEquals(orig.getRoutes().size(), actual.getRoutes().size());
+    assertThat(actual.getRoutes().get(0), is(instanceOf(Route.class)));
+    assertThat(actual.getRoutes().get(0).getStreams().get(0), is(instanceOf(Stream.class)));
   }
 
   private static class Utility {
