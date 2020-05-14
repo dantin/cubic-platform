621a42abc4207c94bbc384fa6dd9b82c45a1ce8d
dantin <chengjie.ding@gmail.com>
2020-05-14 14:45:49 +0800

    add integration test.

--- services/gateway-service/src/test/java/com/github/dantin/cubic/gateway/GatewayIntegrationTest.java
+++ services/gateway-service/src/test/java/com/github/dantin/cubic/gateway/GatewayIntegrationTest.java
@@ -1,51 +1,94 @@
 package com.github.dantin.cubic.gateway;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.Ignore;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.MediaType;
 
 // need oauth2-service to be running.
-@Ignore
+// @Ignore
 public class GatewayIntegrationTest {
 
-  @Test
-  public void whenServiceStartsAndLoadsRealmConfiguration_thenOidcDiscoveryEndpointIsAvailable() {
-    String oidcDiscoveryUrl =
-        "http://localhost:8083/auth/realms/ultrasound/.well-known/openid-configuration";
-
-    Response response = RestAssured.given().redirects().follow(false).get(oidcDiscoveryUrl);
-
-    assertThat(HttpStatus.OK.value()).isEqualTo(response.getStatusCode());
-    System.out.println(response.asString());
-    assertThat(response.jsonPath().getMap("$."))
-        .containsKeys("issuer", "authorization_endpoint", "token_endpoint", "userinfo_endpoint");
+  @BeforeClass
+  public static void init() {
+    RestAssured.baseURI = "http://localhost";
+    RestAssured.port = 8080;
   }
 
   @Test
-  public void givenAuthorizationCodeGrant_whenObtainAccessToken_thenSuccess() {
-    String accessToken = obtainAccessToken();
+  public void doLogin_thenSuccess() {
+    String accessToken = obtainAccessToken("room01", "password");
     assertThat(accessToken).isNotBlank();
   }
 
-  private String obtainAccessToken() {
-    String tokenUrl = "http://localhost:8083/auth/realms/ultrasound/protocol/openid-connect/token";
-    // obtain authentication url with custom code
-    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-    params.add("client_id", "ultrasound_api_service");
-    params.add("grant_type", "password");
-    params.add("client_secret", "3d973f0c-d0ed-43b9-8c13-67f5ea73e335");
-    params.add("scope", "read");
-    params.add("username", "room01");
-    params.add("password", "password");
+  @Test
+  public void retrieveRoom_thenSuccess() {
+    String accessToken = obtainAccessToken("room01", "password");
 
-    Response response = RestAssured.given().formParams(params).post(tokenUrl);
-    System.out.println(response.asString());
+    String url = "/ultrasound/room";
+    Response response =
+        RestAssured.given()
+            .header("Authorization", "Bearer " + accessToken)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .get(url);
+
+    assertThat(HttpStatus.OK.value()).isEqualTo(response.getStatusCode());
+    assertThat(response.jsonPath().getMap("$.")).containsKeys("id", "room", "streams");
+  }
+
+  @Test
+  public void listRoom_thenSuccess() {
+    String accessToken = obtainAccessToken("admin", "password");
+
+    String url = "/ultrasound/room/list";
+    Response response =
+        RestAssured.given()
+            .header("Authorization", "Bearer " + accessToken)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .get(url);
+
+    assertThat(HttpStatus.OK.value()).isEqualTo(response.getStatusCode());
+    assertThat(response.jsonPath().getMap("$."))
+        .containsKeys("pages", "page", "total", "size", "has_previous", "has_next", "routes");
+
+    // list by page
+    response =
+        RestAssured.given()
+            .header("Authorization", "Bearer " + accessToken)
+            .queryParam("s", "4")
+            .queryParam("n", "1")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .get(url);
+
+    assertThat(HttpStatus.OK.value()).isEqualTo(response.getStatusCode());
+    assertThat(response.jsonPath().getMap("$."))
+        .containsKeys("pages", "page", "total", "size", "has_previous", "has_next", "routes");
+    assertThat(response.jsonPath().getString("size")).isEqualTo("4");
+    assertThat(response.jsonPath().getString("page")).isEqualTo("1");
+    assertThat(response.jsonPath().getString("pages")).isEqualTo("2");
+  }
+
+  private String obtainAccessToken(String username, String password) {
+    JSONObject json = null;
+    try {
+      json = new JSONObject().put("username", username).put("password", password);
+    } catch (JSONException e) {
+      assertNull(e);
+    }
+
+    String url = "/ultrasound/auth/login";
+    Response response =
+        RestAssured.given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(json.toString())
+            .post(url);
     return response.jsonPath().getString("access_token");
   }
 }
---
+++ services/oauth2-service/src/test/java/com/github/dantin/cubic/oauth2/AuthApplicationIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.github.dantin.cubic.oauth2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+// need oauth2-service to be running.
+@Ignore
+public class AuthApplicationIntegrationTest {
+
+  @Test
+  public void whenServiceStartsAndLoadsRealmConfiguration_thenOidcDiscoveryEndpointIsAvailable() {
+    String oidcDiscoveryUrl =
+        "http://localhost:8083/auth/realms/ultrasound/.well-known/openid-configuration";
+
+    Response response = RestAssured.given().redirects().follow(false).get(oidcDiscoveryUrl);
+
+    assertThat(HttpStatus.OK.value()).isEqualTo(response.getStatusCode());
+    System.out.println(response.asString());
+    assertThat(response.jsonPath().getMap("$."))
+        .containsKeys("issuer", "authorization_endpoint", "token_endpoint", "userinfo_endpoint");
+  }
+
+  @Test
+  public void givenAuthorizationCodeGrant_whenObtainAccessToken_thenSuccess() {
+    String accessToken = obtainAccessToken();
+    assertThat(accessToken).isNotBlank();
+  }
+
+  private String obtainAccessToken() {
+    String tokenUrl = "http://localhost:8083/auth/realms/ultrasound/protocol/openid-connect/token";
+    // obtain authentication url with custom code
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("client_id", "ultrasound_api_service");
+    params.add("grant_type", "password");
+    params.add("client_secret", "3d973f0c-d0ed-43b9-8c13-67f5ea73e335");
+    params.add("scope", "read");
+    params.add("username", "room01");
+    params.add("password", "password");
+
+    Response response = RestAssured.given().formParams(params).post(tokenUrl);
+    System.out.println(response.asString());
+    return response.jsonPath().getString("access_token");
+  }
+}
