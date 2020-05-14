b5e27dfcc28e295d9ec562518dafe4b2871272f1
dantin <chengjie.ding@gmail.com>
2020-05-14 15:38:12 +0800

    add integration test.

--- services/gateway-service/src/test/java/com/github/dantin/cubic/gateway/GatewayApplicationTest.java
+++ services/gateway-service/src/test/java/com/github/dantin/cubic/gateway/GatewayApplicationTest.java
@@ -1,17 +1,19 @@
 package com.github.dantin.cubic.gateway;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(
     value = {"eureka.client.enabled:false"},
     webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@Ignore
 public class GatewayApplicationTest {
 
   @Test
--- services/gateway-service/src/test/java/com/github/dantin/cubic/gateway/GatewayIntegrationTest.java
+++ services/gateway-service/src/test/java/com/github/dantin/cubic/gateway/GatewayIntegrationTest.java
@@ -1,25 +1,61 @@
 package com.github.dantin.cubic.gateway;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import com.github.dantin.cubic.base.CollectionsHelper;
+import com.github.dantin.cubic.protocol.chat.ChatMessage;
+import com.github.dantin.cubic.protocol.chat.ChatMessage.MessageType;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 // need all services to be running.
-// @Ignore
+@Ignore
 public class GatewayIntegrationTest {
+
+  private static final String SEND_STATUS_ENDPOINT = "/api/message.joinUser";
+  private static final String SEND_MESSAGE_ENDPOINT = "/api/message.sendMessage";
+  private static final String SUBSCRIBE_STATUS_ENDPOINT = "/topic/public";
+  private static final String SUBSCRIBE_MESSAGE_ENDPOINT = "/topic/public";
+
+  private String endpoint;
+  private CompletableFuture<ChatMessage> completableFuture;
 
   @BeforeClass
   public static void init() {
     RestAssured.baseURI = "http://localhost";
     RestAssured.port = 8080;
+  }
+
+  @Before
+  public void setUp() {
+    this.completableFuture = new CompletableFuture<>();
+    this.endpoint = "ws://localhost:8080/chat/ws";
   }
 
   @Test
@@ -123,5 +159,45 @@ public class GatewayIntegrationTest {
             .body(json.toString())
             .post(url);
     return response.jsonPath().getString("access_token");
+  }
+
+  @Test
+  public void sendStatusEndpoint_thenSuccess()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    WebSocketStompClient stompClient =
+        new WebSocketStompClient(new SockJsClient(createTransportClient()));
+    stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+    StompSession stompSession =
+        stompClient.connect(endpoint, new StompSessionHandlerAdapter() {}).get(3, TimeUnit.SECONDS);
+
+    ChatMessage body = new ChatMessage();
+    body.setType(MessageType.JOIN);
+    body.setSender("test-user");
+    body.setContent("{\"content\": \"json content\"}");
+
+    stompSession.subscribe(SUBSCRIBE_STATUS_ENDPOINT, new ChatMessageStompFrameHandler());
+    stompSession.send(SEND_STATUS_ENDPOINT, body);
+
+    ChatMessage chatMessage = completableFuture.get(10, TimeUnit.SECONDS);
+    assertNotNull(chatMessage);
+  }
+
+  private List<Transport> createTransportClient() {
+    return CollectionsHelper.listOf(new WebSocketTransport(new StandardWebSocketClient()));
+  }
+
+  @SuppressWarnings("NullableProblems")
+  private class ChatMessageStompFrameHandler implements StompFrameHandler {
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+      return ChatMessage.class;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+      completableFuture.complete((ChatMessage) payload);
+    }
   }
 }
